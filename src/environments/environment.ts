c01e7018d7d1db0dc82b6a962fbb13d0be9cd1e4
Ekrem007 <ekremugur_07@hotmail.com>
2026-05-21 00:21:08 +0300

    aab

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  apiUrl: 'https://ekrem0707-001-site1.rtempurl.com'
+  apiUrl: 'http://ekrem0707-001-site1.rtempurl.com'
   //apiUrl: 'https://localhost:7092'
   
 };
